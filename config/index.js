59947885541d3fef86f0a02601108dcd59aa21da
Manoj Chauhan <63840371+manojchauhan22@users.noreply.github.com>
2020-10-16 15:55:44 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT,
   db: {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
-    username: process.env.DB_USER || 'oro_admin',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'patient',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     dialect: process.env.DB_DIALECT || 'postgres',
     pool: {
 			max: 5,
